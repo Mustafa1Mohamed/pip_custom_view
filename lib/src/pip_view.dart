e9b3aeff1ee8147188f4371cbf14f830f88cb41f
Mohamed Elsafty <35858989+mohamedegy107@users.noreply.github.com>
2025-12-22 21:49:20 +0200

    Refactor PIPView class and state management

--- lib/src/pip_view.dart
+++ lib/src/pip_view.dart
@@ -1,3 +1,45 @@
+// widgets/pip_view.dart
+import 'package:flutter/material.dart';
+// import '../models/pip_state.dart';
+// import '../navigation/pip_navigation_handler.dart';
+import 'dismiss_keyboard.dart';
+import 'helpers/pip_controllers.dart';
+import 'helpers/pip_navigation_services.dart';
+import 'raw_pip_view.dart';
+
+// models/pip_state.dart
+enum PipViewState { expanded, floating }
+
+class PIPView extends StatefulWidget {
+  final PIPViewCorner initialCorner;
+  final double? floatingWidth;
+  final double? floatingHeight;
+  final bool avoidKeyboard;
+  final Widget pipViewWidget;
+  final Widget Function(BuildContext context, bool isFloating) builder;
+  final GlobalKey<NavigatorState>? parentNavigatorKey;
+  final Route<dynamic> Function(RouteSettings) routes;
+
+  const PIPView({
+    Key? key,
+    required this.builder,
+    required this.pipViewWidget,
+    this.initialCorner = PIPViewCorner.topRight,
+    this.floatingWidth,
+    this.floatingHeight,
+    this.avoidKeyboard = true,
+    this.parentNavigatorKey,
+    required this.routes,
+  }) : super(key: key);
+
+  @override
+  PIPViewState createState() => PIPViewState();
+
+  static PIPController? of(BuildContext context) {
+    return context.findAncestorStateOfType<PIPViewState>();
+  }
+}
+
 class PIPViewState extends State<PIPView>
     with TickerProviderStateMixin, AutomaticKeepAliveClientMixin
     implements PIPController {
